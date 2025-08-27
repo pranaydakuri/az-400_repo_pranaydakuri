2638c742099b0b6144afb8b062526fab8114ec29
pranaydakuri <dpranayreddy4811@gmail.com>
2025-08-27 15:31:12 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
